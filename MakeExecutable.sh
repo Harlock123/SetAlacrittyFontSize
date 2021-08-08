2a28c596629e122d706bef069e5b48a5148e86e2
Lonnie Watson <harlock123@gmail.com>
2021-08-08 09:24:04 -0400

    Added Make Script to single file compile int Releases

---
+++ MakeExecutable.sh
@@ -0,0 +1,6 @@
+rm -r Releases
+mkdir Releases
+
+dotnet publish --runtime linux-x64 --configuration Release /p:PublishSingleFile=true /p:PublishTrimmed=true
+
+cp bin/Release/net5.0/linux-x64/publish/SetAlacrittyFontSize Releases
